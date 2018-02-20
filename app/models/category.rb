62eacd843c103abc1eaeda3ed54b2d2b2b5c8700
Davidalen Fountain <dfountain@sacent.com>
2018-02-20 03:13:40 +0000

    add article and category association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+  
   # Validates the presence of a name, that it is unique, and is atleast 3 characters and less than 25
   validates :name, presence: true,
             uniqueness: { case_sensitive: false },
